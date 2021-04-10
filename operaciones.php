14fefa9db7652a9f348bdfc4cc8d7ea0d44b1a86
Lucas Exequiel Pérez Maidana <lucasexe91@hotmail.com>
2021-04-10 11:09:02 -0300

    dividir verifica operando cero

--- operaciones.php
+++ operaciones.php
@@ -7,6 +7,10 @@
         return $op1 - $op2
     }
     function division($op1,$op2){
+        if ($op2 == 0){
+            echo 'Error: no se puede dividir por 0';
+            die();
+        }
         return $op1 / $op2
     }
     function multiplicacion($op1,$op2){
